39e14e16fb9590012ad48a3d6e7a8766ba73f8b6
Vyacheslav Bolotov <tier56@gmail.com>
2023-05-10 07:32:09 +0500

    Создан компонент ShopItemClass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import './main.css';
+import { ShopItemClass } from './components';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
     </div>
   );
 }
